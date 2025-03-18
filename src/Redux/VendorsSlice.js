c177a4ebf98510204bc61b0d5528cd73fd282ed0
Vishnupriyan459 <vpriyan459@gmail.com>
2025-03-18 19:29:30 +0530

    Update VendorsSlice.js

--- src/Redux/VendorsSlice.js
+++ src/Redux/VendorsSlice.js
@@ -5,7 +5,8 @@ import axios from 'axios';
 // Thunk for fetching vendors and their products
 export const fetchVendors = createAsyncThunk('vendors/fetchVendors', async () => {
   try {
-    const response = await axios.get('/Data/vendor.json');
+    const response = await axios.get(`/Data/vendor.json`);
+    console.log('Fetched orders:', action.payload); 
     return response.data.vendors; // Adjust this to the correct structure
   } catch (error) {
     throw new Error('Failed to fetch vendors: ' + error.message);
